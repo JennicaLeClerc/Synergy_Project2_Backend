a42d72194d750ef489a05bc3236aa30c7b192f24
rphaynes <ryan.haynes@revature.net>
2021-11-29 16:50:40 -0500

    Spring Start

---
+++ src/main/java/com/revature/shms/SynergyHotelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature.shms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyHotelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyHotelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/shms/SynergyHotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.shms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyHotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
