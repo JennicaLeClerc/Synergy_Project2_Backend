a5edabffa9bca6aa25aea215da47ee4a42b969b1
luisloustaunau <luis62civil@gmail.com>
2021-11-30 13:47:12 -0600

    adedded reservation

--- src/main/java/com/revature/shms/models/Reservation.java
+++ src/main/java/com/revature/shms/models/Reservation.java
@@ -1,11 +1,14 @@
 package com.revature.shms.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Reservation model
@@ -21,11 +24,16 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.AUTO)
     int reservationId;
 
-    @Column
-    String userId;
 
     @Column
     String status;
 
-    
+    @Column
+    Date date;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonBackReference
+    private User userReserve;
+
+
 }
--- src/main/java/com/revature/shms/models/User.java
+++ src/main/java/com/revature/shms/models/User.java
@@ -1,18 +1,21 @@
 package com.revature.shms.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	int userID;
+	int userId;
 
 	String firstName;
 	String lastName;
@@ -21,5 +24,10 @@ public class User {
 	String userName;
 	@Column(nullable = false)
 	String password;
+
+//    @OneToMany(mappedBy = "userReserve")
+//	@JsonManagedReference
+//	private List<Reservation> reservations;
+
 
 }
