6d3e7d0f5a6774a3ec807a9660a52d8eeeebecac
Ernst Aine <vilycharles72@gmail.com>
2021-11-30 15:20:25 -0500

    create a method on UserRepository and use that method on UserService to check if userName and password exists in the database in order to log in user

--- src/main/java/com/revature/shms/services/UserService.java
+++ src/main/java/com/revature/shms/services/UserService.java
@@ -2,7 +2,13 @@ package com.revature.shms.services;
 
 import com.revature.shms.models.User;
 import com.revature.shms.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service // this annotation is to denote that this is a service class for user
 public class UserService{
@@ -12,9 +18,19 @@ public class UserService{
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+    //this method create a new user and add it to the repository
     public User createNewUser(User user ){
         return userRepository.save(user);
     }
+
+    public boolean login(){
+
+        return false;
+    }
+
+
+
 
 
 
