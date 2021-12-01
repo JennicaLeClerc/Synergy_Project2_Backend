3cfcecebf053f60d8b6eb1cb34ea84320ac40142
JennicaLeClerc <93157746+JennicaLeClerc@users.noreply.github.com>
2021-12-01 15:40:56 -0500

    Update ReservationRepository.java

--- src/main/java/com/revature/shms/repositories/ReservationRepository.java
+++ src/main/java/com/revature/shms/repositories/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.revature.shms.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Integer > {
 
 }
