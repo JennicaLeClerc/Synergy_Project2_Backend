927972bfb2b7bf0f5f66dfc6ef0af15faf8cfbe7
Jennica LeClerc <jennica@MacBook-Air.local>
2021-12-02 17:11:42 -0500

    Added comments to User Respository

--- src/main/java/com/revature/shms/services/UserService.java
+++ src/main/java/com/revature/shms/services/UserService.java
@@ -1,6 +1,5 @@
 package com.revature.shms.services;
 
-import com.revature.shms.models.Employee;
 import com.revature.shms.models.User;
 import com.revature.shms.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -25,42 +24,65 @@ public class UserService{
     @Autowired
     private UserRepository userRepository;
 
-    //this method create a new user and add it to the repository
+    /**
+     * Saves the user to the database.
+     * @param user the user to be saved.
+     * @return the User, but not saved to the database.
+     */
     public User createNewUser(User user ){
         return userRepository.save(user);
     }
 
-    /*
-    * if the userName and password exists in the repository return true and log in the user
-    * otherwise return false
+    /**
+     * Logs in the user with the given username and password, then returns that User.
+     * @param userName the username to match.
+     * @param password the password to match.
+     * @return User of the given username AND password.
+     * @throws AccessDeniedException if the username AND password aren't in the database together this will be thrown.
      */
     public User login(String userName,String password) throws AccessDeniedException {
 		try {
-			User user = getUserByUserName(userName);
+			User user = getUserByUsername(userName);
 			if (user.getPassword().equals(password)) return user;
 		} catch (NotFound e) { throw new AccessDeniedException("Incorrect username/password");}
 		throw new AccessDeniedException("Incorrect username/password");
 	}
 
-    /*
-    when the user click via link or button via a get request to logout
-    this method redirect the user to a the log out page
+    /**
+     * Logs out the user and redirect them to the logout page.
+     * @return String that redirects the user to the logout page.
      */
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(){
         return "redirect:logoutPage";
     }
 
+    /**
+     * Gets a list of all users.
+     * @return List<User> for all users in the database ordered by UserID in descending order.
+     */
     public List<User> getAllUsers(){
         return userRepository.findAllByOrderByUserIDDesc();
     }
 
-    public User getUserByUserName(String userName) throws NotFound {
-        return userRepository.findByUsername(userName).orElseThrow(NotFound::new);
+    /**
+     * Get the user with the given username.
+     * @param username the username to match.
+     * @return User with the given username.
+     * @throws NotFound if the username is not in the database this will be thrown.
+     */
+    public User getUserByUsername(String username) throws NotFound {
+        return userRepository.findByUsername(username).orElseThrow(NotFound::new);
     }
 
-    public User getUserByUserId(int userId) throws NotFound {
-        return userRepository.findByUserID(userId).orElseThrow(NotFound::new);
+    /**
+     * Get the user with the given user ID.
+     * @param userID the user ID to match.
+     * @return User with the given userID.
+     * @throws NotFound if the userID is not in the database this will be thrown.
+     */
+    public User getUserByUserID(int userID) throws NotFound {
+        return userRepository.findByUserID(userID).orElseThrow(NotFound::new);
     }
 }
 
