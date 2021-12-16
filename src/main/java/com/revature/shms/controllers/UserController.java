ca5bcd812c7406cf637dd9d5e608d59dd71d6703
Jennica LeClerc <jennica@MacBook-Air.local>
2021-12-15 21:04:46 -0500

    new user endpoints

--- src/main/java/com/revature/shms/controllers/UserController.java
+++ src/main/java/com/revature/shms/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -18,6 +19,10 @@ public class UserController {
     private UserRepository userRepository;
     @Autowired
     private UserService userService;
+    private int userID;
+    private String firstname;
+    private String lastName;
+    private String email;
 
     @GetMapping("/{userID}")
     public ResponseEntity<?> findUserById(@PathVariable int userID) {
@@ -34,5 +39,23 @@ public class UserController {
                                             @RequestBody HashMap<String, String> password) {
         return ResponseEntity.ok(userService.updatePassword(userID, password.get("old"),
                 password.get("new")));
+    }
+
+    @PutMapping("/firstName/{userID}")
+    public ResponseEntity<?> updateFirstName(@PathVariable int userID,
+                                        @RequestBody String firstName){
+        return ResponseEntity.ok(userService.updateFirstName(userID, firstName));
+    }
+
+    @PutMapping("/lastName/{userID}")
+    public ResponseEntity<?> updateLastName(@PathVariable int userID,
+                                        @RequestBody String lastName){
+        return ResponseEntity.ok(userService.updateLastName(userID, lastName));
+    }
+
+    @PutMapping("/email/{userID}")
+    public ResponseEntity<?> updateEmail(@PathVariable int userID,
+                                            @RequestBody String email){
+        return ResponseEntity.ok(userService.updateEmail(userID, email));
     }
 }
